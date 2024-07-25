2bb1752cac7c7591cd261aa121ba785e5133d8fa
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-24 22:36:04 -0400

    working department model. have not yet made migrations

--- mass_notification_api/departments/models.py
+++ mass_notification_api/departments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True, blank=False)
+    head_of_department = models.CharField(max_length=100, blank=False)
+    phone_number = PhoneNumberField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
