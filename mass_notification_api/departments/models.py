e1299c17c5ed8f833d6d78af80d9392abf7954b3
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-26 01:53:43 -0400

    added title field to CustomUser. removed uneccessary blank attributes in Department model

--- mass_notification_api/departments/models.py
+++ mass_notification_api/departments/models.py
@@ -3,7 +3,7 @@ from users.models import CustomUser
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Department(models.Model):
-    name = models.CharField(max_length=100, unique=True, blank=False)
+    name = models.CharField(max_length=100, unique=True)
     head_of_department = models.ForeignKey(CustomUser, related_name="departments", on_delete=models.CASCADE)
     phone_number = PhoneNumberField(blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
--- mass_notification_api/users/models.py
+++ mass_notification_api/users/models.py
@@ -8,21 +8,21 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     # PermissionsMixin automatically includes 'is_superuser' to the CustomUser model. 'is_superuser = models.BooleanField(default=False)' is not neccessary.
-    first_name = models.CharField(max_length=40, blank=False)
-    last_name = models.CharField(max_length=40, blank=False)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    title = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone_number = PhoneNumberField(blank=True, null=True)
-    #department field needed
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     # 'objects' is used to define a manager for a model. These creates an instance of custom user manager class to perform queries.
     objects = CustomUserManager()
-    
+
     USERNAME_FIELD = 'email'
     ## REQUIRED_FIELDS is only related to superuser creation. Django will prompt for these fields in addition to the USERNAME_FIELD
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
-    def __str__(self):
-        return self.email
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name}\n{self.title}\n{self.email}\n{self.phone_number}\nDate joined: {self.date_joined}\n{self.is_active}'
