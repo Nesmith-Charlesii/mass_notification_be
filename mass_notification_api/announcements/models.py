2d0e094750975fbb8e6c05289c50da037a938e23
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-27 17:02:52 -0400

    created announcements model and updated roles model

---
+++ mass_notification_api/announcements/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users.models import CustomUser
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    author = models.ForeignKey(CustomUser, related_name='announcements')
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f'Title: {self.title}\nMessage: {self.message}\nAuthor: {self.author}\nDate created: {self.date_created}\nLast Modified: {self.last_modified}'
