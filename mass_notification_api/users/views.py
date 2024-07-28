66de84acb3bfc4fc5ec1bb945dbcb1977278e627
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-28 17:17:59 -0400

    tested user queryset in postman. GET and POST good to go.

--- mass_notification_api/users/views.py
+++ mass_notification_api/users/views.py
@@ -1,3 +1,6 @@
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import CustomUserSerializer
