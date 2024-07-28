0dac6eee5606aa53c8a4dc490f2868e15cf99d56
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-27 23:14:02 -0400

    created user serializer. started viewset business logic

--- mass_notification_api/users/serializer.py
+++ mass_notification_api/users/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
 
-class UserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = [
