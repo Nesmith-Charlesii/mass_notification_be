98100e26c0dcf960b50e50f455a68a7ef2d52104
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-28 16:43:45 -0400

    added basename attribute to router.register in users.urls to fix duplicate 'users' appended to url path

--- mass_notification_api/users/urls.py
+++ mass_notification_api/users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('/users', include(router.urls))
+    path('', include(router.urls))
 ]
