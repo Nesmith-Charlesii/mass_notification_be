543f50ea803e84fbb4227639f1a169a528b2705a
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-28 16:34:01 -0400

    setup base viewset for CustomUser model and routes

--- mass_notification_api/users/views.py
+++ mass_notification_api/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 from .models import CustomUser
-from .serializer import CustomUserSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from .serializers import CustomUserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
