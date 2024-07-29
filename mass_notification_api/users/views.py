c7a3c5cda48253372f2f2e230904a008bab2ebc7
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-29 10:57:14 -0400

    imported .csv of user data

--- mass_notification_api/users/views.py
+++ mass_notification_api/users/views.py
@@ -1,10 +1,11 @@
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
-from rest_framework import viewsets
-from .models import CustomUser
-from .serializers import CustomUserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    
