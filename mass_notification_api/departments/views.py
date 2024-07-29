8512c087f16e94210788470601e03eb9f42b4345
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-28 23:53:11 -0400

    base department viewset

---
+++ mass_notification_api/departments/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from models import Department
+from serializers import DepartmentSerializer
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
