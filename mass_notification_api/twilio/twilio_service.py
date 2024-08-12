f7dae5b873f617d15099dad2f850b4cbc2559deb
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-08-12 16:43:46 -0400

    created .env. updated settings.py

--- mass_notification_api/twilio/twilio_service.py
+++ mass_notification_api/twilio/twilio_service.py
@@ -1,12 +1,11 @@
+from django.conf import settings
 from twilio.rest import Client
 
-account_sid = 'ACd40bf12b3efbc9aec638e752315817c5'
-auth_token = '6ea1346a764e27882f7480c6cd81e56a'
-client = Client(account_sid, auth_token)
+client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
 message = client.messages.create(
   from_= '+18335320199',
-  to= '+19198021911'
+  to= settings.TWILIO_TO_NUMBER
 )
 
 print(message.sid)
