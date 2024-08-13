7bebceb18ec9d0a2b9aed6864d0b5db040945c63
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-08-13 18:46:13 -0400

    added comments describing viewset in user views

--- mass_notification_api/twilio/twilio_service.py
+++ mass_notification_api/twilio/twilio_service.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from twilio.rest import Client
 
-client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+def send_sms(user):
+  client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
-message = client.messages.create(
-  from_= '+18335320199',
-  to= settings.TWILIO_TO_NUMBER
-)
+  message = client.messages.create(
+    from_= '+18335320199',
+    to= settings.TWILIO_TO_NUMBER
+  )
 
-print(message.sid)
+  print(message.sid)
--- mass_notification_api/users/views.py
+++ mass_notification_api/users/views.py
@@ -1,10 +1,21 @@
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
 
-class UserViewSet(viewsets.ModelViewSet):
+import twilio.twilio_service
+
+class UserViewSet(viewsets.ModelViewSet): 
+    '''
+    The queryset defines the data that the viewset will work with. In this case, all user objects are available to the viewsets module
+    The viewset will use this queryset to perform various CRUD operations.
+    '''
     queryset = CustomUser.objects.all()
+
+    '''This tells the viewset which serializer to use when converting CustomUser instances to JSON (or other formats) and vice versa'''
     serializer_class = CustomUserSerializer
+
+    @action(detail=False, methods=['get', 'post'], url_path='nofify_user')
+    def notify_user(self, request):
+        pass
